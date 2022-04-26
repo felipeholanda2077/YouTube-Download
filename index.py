7718d1f8a2e053cf12ef96fafcdb453f12aac02d
Felipe Holanda de Freitas <79938668+felipeholanda2077@users.noreply.github.com>
2022-04-26 13:28:22 -0300

    Update index.py

--- index.py
+++ index.py
@@ -3,3 +3,17 @@ link = input("Enter URL of Video")
 video = YouTube(link)
 stream = video.streams.get_highest_resolution()
 stream.download()
+
+ou
+
+from pytube import YouTube, streams
+
+url = input(str('Url para download: '))
+
+video = YouTube(url)
+
+stream = video.streams.get_highest_resolution()
+
+stream.download(output_path='C:/Users/felip/OneDrive/Documentos/YouTube-Abaixador')
+
+print('Download concluido com sucesso!')
